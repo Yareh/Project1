a50277c65d1c3bd3d255a9723d640a06c2998e11
Alexis Ortiz Rosero <ortizal@byui.edu>
2023-10-17 18:54:06 -0500

    UpDate templeSwagger documentation

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 app.use(express.json());
 const { ConectionDb } = require("./DB/conection");
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 //prevent No 'Access-Control-Allow-Origin'
-app.use((req, res, next) => {
+ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
+ 
 
 // database connection
 ConectionDb((err) => {
